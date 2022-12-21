1934aae1a7ea006d70c8f72488b5ce7a7ea4eb1d
jm111222233312312 <114123450+jm111222233312312@users.noreply.github.com>
2022-12-21 16:24:34 +0900

    Delete main directory

--- main/java/petcare/repository/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package petcare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import petcare.model.FileBoard;
-
-public interface FileRepository extends JpaRepository<FileBoard, Long>{
-
-}
